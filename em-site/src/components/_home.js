8bb457832f5ac1b81b81d27f2b2a1e6549967a76
Adalberto Sampaio <sampaio.afn@gmail.com>
2020-06-22 00:36:18 -0300

    Added example scroll animation

--- em-site/src/components/_home.js
+++ em-site/src/components/_home.js
@@ -1,25 +1,28 @@
 import React from "react"
+import { motion, useViewportScroll, useTransform } from "framer-motion"
 import homeStyles from "./home.module.scss"
 import homeVideo from "../assets/videos/mobilidade.mp4"
 
-const Hero = () => {
+function Hero() {
+  const { scrollYProgress } = useViewportScroll()
+  const transitionY = useTransform(scrollYProgress, [0, 0.5, 1], [0, 0.5, 1])
+
   return (
     <div className={homeStyles.homeLayout}>
       <video className={homeStyles.homeVideo} autoPlay muted loop>
         <source src={homeVideo} type="video/mp4"></source>
       </video>
-      <div className={homeStyles.overlay}>
+      <motion.div className={homeStyles.overlay} style={{ opacity: transitionY }} transition={{ ease: [0.17, 0.67, 0.83]}}>
         <h1>Especial de mobilidade</h1>
         <h2>
           Ícone da arquitetura mundial e maior cidade do mundo construída no
-          século XX, <br />Brasília não se destaca apenas por esses títulos
+          século XX, <br />
+          Brasília não se destaca apenas por esses títulos
         </h2>
         <div className={homeStyles.footer}>
-          <a href="#intro">
-            Leia o especial
-          </a>
+          <a href="#intro">Leia o especial</a>
         </div>
-      </div>
+      </motion.div>
     </div>
   )
 }
