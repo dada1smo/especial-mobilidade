732e3c080b895cfce7a9b7fbe908358779268239
Adalberto Sampaio <sampaio.afn@gmail.com>
2020-07-14 20:48:22 -0300

    Improved quote sub

--- em-site/src/pages/index.js
+++ em-site/src/pages/index.js
@@ -237,7 +237,9 @@ export default function Home() {
           <section className="comparative">
             <div className="third">
               <h3>{Content.s5.c5.question}</h3>
-              <h5 className="quote-sub">{Content.s5.c5.sub}</h5>
+              <div className="quote-sub">
+                <h5>{Content.s5.c5.sub}</h5>
+              </div>
               <div className="col">
                 <Quote
                   quote={Content.s5.c5.h1.quote}
